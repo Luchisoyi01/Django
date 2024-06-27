5c772b54f0466aac170655b3b2e57cc9957ad772
Luchisoyi01 <117387787+Luchisoyi01@users.noreply.github.com>
2024-06-27 22:17:44 +0300

    made some few changes

--- Django_practice/hello/views.py
+++ Django_practice/hello/views.py
@@ -10,6 +10,6 @@ def index (request):
   return HttpResponse(f"hello, {name.capitalize()}!")
 '''
 def greet(request, name):
-    return render(request, "hello/greet.html", {
-        "name": name.capitalize()
-    })
+  return render(request, "hello/greet.html", {
+    "name": name.capitalize()
+  })
